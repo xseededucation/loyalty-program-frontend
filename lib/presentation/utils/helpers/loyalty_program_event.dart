953dba7d2261d664c0f999aa5fa91633defd26f2
Narendran <narendran.i@xseededucation.com>
2023-08-24 15:14:37 +0530

    update inter

--- lib/presentation/utils/helpers/loyalty_program_event.dart
+++ lib/presentation/utils/helpers/loyalty_program_event.dart
@@ -6,15 +6,16 @@ import 'package:intl/intl.dart';
 
 class LoyaltyProgramEvent {
   Timer? _timer;
-  SharedPreferences? sharedPreferences;
+  SharedPreferences? _sharedPreferences;
 
   Future<void> initialize() async {
-    sharedPreferences = await SharedPreferences.getInstance();
+    _sharedPreferences = await SharedPreferences.getInstance();
   }
 
   void appOpened() async {
     String currentDate = _getCurrentDate();
-    if (await _getAppLastOpenedDate() != currentDate) {
+    String lastOpenedDate = _getAppLastOpenedDate();
+    if (lastOpenedDate != currentDate) {
       _setLastOpenedDate(currentDate);
       _clearTimeBoundPref();
       _triggerApiCall(APP_OPEN);
@@ -48,108 +49,31 @@ class LoyaltyProgramEvent {
     });
   }
 
-  Future<void> _triggerApiCall(String event) async {}
+  Future<void> _triggerApiCall(String event) async {
+    print('TRIGGER API CALL $event');
+  }
 
   void _clearTimeBoundPref() {
-    sharedPreferences!.remove(SHARED_PREF_LAST_TIME_BOUND);
+    _sharedPreferences!.remove(SHARED_PREF_LAST_TIME_BOUND);
   }
 
   int _getLasTimerInSeconds() {
-    return sharedPreferences!.getInt(SHARED_PREF_LAST_TIME_BOUND) ?? 0;
+    return _sharedPreferences!.getInt(SHARED_PREF_LAST_TIME_BOUND) ?? 0;
   }
 
   void _setLastOpenedDate(String currentDate) {
-    sharedPreferences!.setString(SHARED_PREF_LAST_OPENED, currentDate);
+    _sharedPreferences!.setString(SHARED_PREF_LAST_OPENED, currentDate);
   }
 
   void _setLastSetTimer(int time) {
-    sharedPreferences!.setInt(SHARED_PREF_LAST_TIME_BOUND, time);
+    _sharedPreferences!.setInt(SHARED_PREF_LAST_TIME_BOUND, time);
   }
 
   String _getAppLastOpenedDate() {
-    return sharedPreferences!.getString(SHARED_PREF_LAST_OPENED) ?? '';
+    return _sharedPreferences!.getString(SHARED_PREF_LAST_OPENED) ?? '';
   }
 
   String _getCurrentDate() {
     return DateFormat('yyyy-MM-dd').format(DateTime.now());
   }
 }
-
-
-// class LoyaltyProgramEvents {
-//   bool appOpened = false;
-//   bool sessionStarted = false;
-//   bool lessonPlanClosed = false;
-
-//   // Timer
-//   Timer? timer;
-//   int counterSeconds = 0;
-//   late DateTime endTime;
-//   int endMinutes = 0;
-//   int endSeconds = 0;
-
-
-//   retrieveAppOpenedStatus() async {
-//     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-//     String currentDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
-
-//     String lastOpenedDate = sharedPreferences.getString('lastOpenedDate') ?? '';
-//     bool appOpened = sharedPreferences.getBool('appOpened') ?? false;
-
-//     print("currentDate : $currentDate");
-
-//     if (currentDate != lastOpenedDate) {
-//       sharedPreferences.setString('lastOpenedDate', currentDate);
-//       sharedPreferences.setBool('appOpened', false);
-//       appOpened = false;
-//     } else {
-//       appOpened = appOpened;
-//     }
-//     return appOpened;
-//   }
-
-//   // Timer
-//   Future<void> startTimer(int minutes) async {
-//     if (timer != null) {
-//       timer!.cancel();
-//     }
-
-//     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-//     int loyaltyTimerSecondsAtCancel =
-//         sharedPreferences.getInt('loyaltyTimerSecondsAtCancel') ?? 0;
-
-//     print("startTimer : loyaltyTimerSecondsAtCancel :  $counterSeconds");
-
-//     endMinutes = minutes;
-//     endSeconds = minutes * 60;
-//     counterSeconds = loyaltyTimerSecondsAtCancel;
-
-//     timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-//       counterSeconds = counterSeconds + 1;
-//       print("startTimer : $counterSeconds");
-
-//       sharedPreferences.setInt('loyaltyTimerSecondsAtCancel', counterSeconds);
-//       if (counterSeconds > endSeconds) {
-//         timer.cancel();
-//         print("Call API to update");
-//       }
-//     });
-//   }
-
-//   Future<void> cancelTimer() async {
-//     if (timer != null) {
-//       timer!.cancel();
-//     }
-//     print("cancelTimer : loyaltyTimerSecondsAtCancel : called");
-//   }
-
-//   Future<void> clearAll() async {
-//     if (timer != null) {
-//       timer!.cancel();
-//     }
-//     print("cancelTimer : loyaltyTimerSecondsAtCancel : called");
-
-//     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-//     sharedPreferences.clear();
-//   }
-// }
